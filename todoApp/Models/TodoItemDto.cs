dc62da40ee35e5081446b92e50eb3a23e6e471a6
RiceBoyy <owen.j.de.guzman@gmail.com>
2024-03-22 18:33:46 +0100

    it is done

--- todoApp/Models/TodoItemDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace todoApp.Models
-{
-    public class TodoItemDto
-    {
-        public int Id { get; set; }
-        public string UserId { get; set; }
-        public string Item { get; set; }
-    }
-}
